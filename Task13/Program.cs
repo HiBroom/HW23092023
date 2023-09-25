8414dad3b7cf73520f6fe15b2806390e333385e6
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-09-25 22:53:08 +0300

    Solved Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int GetThirdRank(int number)
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ThirdNumberIsNotExiting(int number)
+{
+    if(number < 100)
+    {
+        Console.WriteLine($"{number} -> Third digit isn't exeting");
+        return false;
+    }
+    return true;
+}
+
+
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int k = GetThirdRank(num);
+if (ThirdNumberIsNotExiting(num))
+{
+    Console.WriteLine($"{num} -> {k}");
+}
+
+
+
+
+
+
+
+    
